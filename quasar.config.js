39d7506152233c486d4389d76f06277761915a0a
HectorCosgalla <valcos98@hotmail.com>
2025-02-03 04:42:48 -0600

    just another small change. let's see if it works!

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/',
+      publicPath: process.env.NODE_ENV === 'production' ? '/ValcosSolutionsLandingPage/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
